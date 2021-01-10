8fd0e2f9fd28f82e6fc9371ad5162a164ed709ca
RezwanFaruque <rezwanfaruque9@gmail.com>
2021-01-11 02:44:12 +0600

    adding bootstrap and sass file

---
+++ src/components/MainComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+     <div class="container">
+       <div class="row">
+         <div class="col-12">
+            <h2>Testing Bootstrap</h2>
+         </div>
+       </div>
+     </div>
+  </div>
+</template>
+
+<script>
+
+// import css file 
+import '../assets/css/style.css';
+export default {
+  name: 'MainComponent',
+  
+}
+</script>
+
