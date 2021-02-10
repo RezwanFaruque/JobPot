32d0f72bf54679c71992a787188d4b66ccbd71b2
RezwanFaruque <rezwanfaruque9@gmail.com>
2021-02-10 17:34:15 +0600

    left sidebar job search done

--- src/components/JobFilterSideBar.vue
+++ src/components/JobFilterSideBar.vue
@@ -1,12 +1,257 @@
 <template>
   <div id="jobfilter_sidebar">
-      This is job filter sidebar
+    <div class="section">
+      <div class="section-title">Type of Employment</div>
+      <hr>
+      <div class="section-body">
+        <b-form>
+          <div class="check-boxes-with-number">
+            <div class="check-box">
+              <b-form-checkbox
+                id="fulltimejob"
+                name="fulltimejob"
+                value="accepted"
+                unchecked-value="not_accepted"
+              >
+                <span>Full Time Jobs</span>
+              </b-form-checkbox>
+            </div>
+            <div class="number">20</div>
+          </div>
+          <div class="check-boxes-with-number">
+            <div class="check-box">
+              <b-form-checkbox
+                
+                value="accepted"
+                unchecked-value="not_accepted"
+              >
+                <span>Part Time Jobs</span>
+              </b-form-checkbox>
+            </div>
+            <div class="number">20</div>
+          </div>
+          <div class="check-boxes-with-number">
+            <div class="check-box">
+              <b-form-checkbox
+                
+                value="accepted"
+                unchecked-value="not_accepted"
+              >
+                <span>Remote job</span>
+              </b-form-checkbox>
+            </div>
+            <div class="number">20</div>
+          </div>
+          <div class="check-boxes-with-number">
+            <div class="check-box">
+              <b-form-checkbox
+               
+                value="accepted"
+                unchecked-value="not_accepted"
+              >
+                <span>Internship Jobs</span>
+              </b-form-checkbox>
+            </div>
+            <div class="number">20</div>
+          </div>
+          <div class="check-boxes-with-number">
+            <div class="check-box">
+              <b-form-checkbox
+               
+                value="accepted"
+                unchecked-value="not_accepted"
+              >
+                <span>Freelance Jobs</span>
+              </b-form-checkbox>
+            </div>
+            <div class="number">20</div>
+          </div>
+          <div class="check-boxes-with-number">
+            <div class="check-box">
+              <b-form-checkbox
+               
+                value="accepted"
+                unchecked-value="not_accepted"
+              >
+                <span>Training Jobs</span>
+              </b-form-checkbox>
+            </div>
+            <div class="number">20</div>
+          </div>
+        </b-form>
+      </div>
+    </div>
+
+  <!-- seniority level -->
+    <div class="section">
+      <div class="section-title">Seniority Level</div>
+      <hr>
+      <div class="section-body">
+        <b-form>
+          <div class="check-boxes-with-number">
+            <div class="check-box">
+              <b-form-checkbox
+                id="fulltimejob"
+                name="fulltimejob"
+                value="accepted"
+                unchecked-value="not_accepted"
+              >
+                <span>Student Level</span>
+              </b-form-checkbox>
+            </div>
+            <div class="number">20</div>
+          </div>
+          <div class="check-boxes-with-number">
+            <div class="check-box">
+              <b-form-checkbox
+                
+                value="accepted"
+                unchecked-value="not_accepted"
+              >
+                <span>Entry Level</span>
+              </b-form-checkbox>
+            </div>
+            <div class="number">20</div>
+          </div>
+          <div class="check-boxes-with-number">
+            <div class="check-box">
+              <b-form-checkbox
+                
+                value="accepted"
+                unchecked-value="not_accepted"
+              >
+                <span>Mid Level</span>
+              </b-form-checkbox>
+            </div>
+            <div class="number">20</div>
+          </div>
+          <div class="check-boxes-with-number">
+            <div class="check-box">
+              <b-form-checkbox
+               
+                value="accepted"
+                unchecked-value="not_accepted"
+              >
+                <span>Senior Level</span>
+              </b-form-checkbox>
+            </div>
+            <div class="number">20</div>
+          </div>
+          <div class="check-boxes-with-number">
+            <div class="check-box">
+              <b-form-checkbox
+               
+                value="accepted"
+                unchecked-value="not_accepted"
+              >
+                <span>Director Level</span>
+              </b-form-checkbox>
+            </div>
+            <div class="number">20</div>
+          </div>
+          <div class="check-boxes-with-number">
+            <div class="check-box">
+              <b-form-checkbox
+               
+                value="accepted"
+                unchecked-value="not_accepted"
+              >
+                <span>VP or above</span>
+              </b-form-checkbox>
+            </div>
+            <div class="number">20</div>
+          </div>
+        </b-form>
+      </div>
+    </div>
+
+<!-- salary range -->
+    <div class="section">
+      <div class="section-title">Salary Range</div>
+      <hr>
+      <div class="section-body">
+        <b-form>
+          <div class="check-boxes-with-number">
+            <div class="check-box">
+              <b-form-checkbox
+                id="fulltimejob"
+                name="fulltimejob"
+                value="accepted"
+                unchecked-value="not_accepted"
+              >
+                <span>$200 - $500</span>
+              </b-form-checkbox>
+            </div>
+            <div class="number">20</div>
+          </div>
+          <div class="check-boxes-with-number">
+            <div class="check-box">
+              <b-form-checkbox
+                
+                value="accepted"
+                unchecked-value="not_accepted"
+              >
+                <span>$500 - $750</span>
+              </b-form-checkbox>
+            </div>
+            <div class="number">20</div>
+          </div>
+          <div class="check-boxes-with-number">
+            <div class="check-box">
+              <b-form-checkbox
+                
+                value="accepted"
+                unchecked-value="not_accepted"
+              >
+                <span>$750 - $1000</span>
+              </b-form-checkbox>
+            </div>
+            <div class="number">20</div>
+          </div>
+          <div class="check-boxes-with-number">
+            <div class="check-box">
+              <b-form-checkbox
+               
+                value="accepted"
+                unchecked-value="not_accepted"
+              >
+                <span>$1000 - $2000</span>
+              </b-form-checkbox>
+            </div>
+            <div class="number">20</div>
+          </div>
+          <div class="check-boxes-with-number">
+            <div class="check-box">
+              <b-form-checkbox
+               
+                value="accepted"
+                unchecked-value="not_accepted"
+              >
+                <span>$2000 - $5000</span>
+              </b-form-checkbox>
+            </div>
+            <div class="number">20</div>
+          </div>
+          <div class="check-boxes-with-number">
+            <div class="check-box">
+              <b-form-checkbox
+               
+                value="accepted"
+                unchecked-value="not_accepted"
+              >
+                <span>$5000+</span>
+              </b-form-checkbox>
+            </div>
+            <div class="number">20</div>
+          </div>
+        </b-form>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-    name: 'JobFilterSidebar'
-
-}
+  name: "JobFilterSidebar",
+};
 </script>
--- src/components/JobSearchBannar.vue
+++ src/components/JobSearchBannar.vue
@@ -5,31 +5,89 @@
       <div class="subtitle">Your dream job is waitng</div>
     </div>
     <div class="search-bar-section">
-      <form>
+      <b-form>
         <div class="search-bar-bannar">
           <div class="search-by keyword">
-
+            <div class="search-icon">
+              <i class="fas fa-search"></i>
+            </div>
+            <div class="input">
+              <b-form-input
+                placeholder="Search by job keyword"
+                required
+              ></b-form-input>
+            </div>
           </div>
           <div class="search-by category">
-
+            <b-form-select
+              v-model="catselected"
+              :options="categories"
+            ></b-form-select>
           </div>
-          <div class="serch-by job-type">
-
+          <div class="search-by job-type">
+            <b-form-select
+              v-model="jobtypeselected"
+              :options="jobtype"
+            ></b-form-select>
           </div>
-          <div class="serch-by postal-code">
-
+          <div class="search-by postal-code">
+            <div class="postal-icon">
+              <i class="fas fa-map-marker-alt"></i>
+            </div>
+            <div class="input">
+              <b-form-input
+                placeholder="City or Postal code"
+                required
+              ></b-form-input>
+            </div>
           </div>
           <div class="submit-button">
             <button class="find-job" type="submit">Find Job</button>
           </div>
         </div>
-      </form>
+      </b-form>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "JobSearchBannar",
+  data: function () {
+    return {
+      catselected: null,
+      jobtypeselected: null,
+      categories: [
+        { value: null, text: "Categories" },
+        { value: "All Creative Job", text: "All Creative Job" },
+        { value: "Bank Job", text: "Bank Job" },
+        { value: "Fashion", text: "Fashion" },
+        { value: "Branding Job", text: "Branding Job" },
+        { value: "Graphics Design Job", text: "Graphics Design Job" },
+        { value: "Illustration Job", text: "Illustration Job" },
+        { value: "UI/UX Design", text: "UI/UX Design" },
+        { value: "Photography", text: "Photography" },
+        { value: "Motion Graphics", text: "Motion Graphics" },
+        { value: "Teacher", text: "Teacher" },
+        { value: "Marketting", text: "Marketting" },
+        { value: "Software Engineering", text: "Software Engineering" },
+        { value: "Web Developer", text: "Web Developer" },
+        { value: "Industrial Design", text: "Industrial Design" },
+        { value: "Interaction Design", text: "Interaction Design" },
+        { value: "Engineering Job", text: "Engineering Job" },
+      ],
+      jobtype: [
+        { value: null, text: "Job Type" },
+        { value: "Full Time Job", text: "Full Time Job" },
+        { value: "Part Time Job", text: "Part Time Job" },
+        { value: "Remote Job", text: "Remote Job" },
+        { value: "Internship Job", text: "Internship Job" },
+        { value: "Freelance Job", text: "Freelance Job" },
+        { value: "Training Job", text: "Training Job" },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
